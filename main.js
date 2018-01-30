3eaef7f9cee7512c0d19560e3aa2ec7c35495897
Gonzalo Ramos <goramos@microsoft.com>
2018-01-29 17:27:46 -0800

    trying to make things simpler / remove code

---
+++ main.js
@@ -0,0 +1,44 @@
+var state = {
+  shortcut: {},
+  methods: [
+    { id: "view", icon: "⬒", title: "Capture Viewport" },
+    { id: "full", icon: "⬛", title: "Capture Document" }
+  ],
+  dpr: [
+    { id: true, title: "Preserve original DPI size" },
+    { id: false, title: "Adjust to actual size" }
+  ]
+};
+
+chrome.commands.getAll((commands) => {
+    var command = commands.filter((command) => command.name === 'take-screenshot')[0]
+    state.shortcut = command.shortcut
+    m.redraw()
+  })  
+
+// read (previously) stored state
+chrome.storage.sync.get(config => {
+  state.methods.forEach(item => {
+    // add member to item marking it as selected or not
+    item.checked = item.id === config.method;
+  });
+  state.dpr.forEach(item => {
+    // add member to item marking it as selected or not
+    item.checked = item.id === config.dpr;
+  });
+});
+
+// set event handlers for changing state
+var events = {
+  method: item => e => {
+    state.methods.forEach(item => (item.checked = false));
+    item.checked = true;
+    // set state
+    chrome.storage.sync.set({ method: item.id });
+  },
+  dpr: item => e => {
+    state.dpr.forEach(item => (item.checked = false));
+    item.checked = true;
+    chrome.storage.sync.set({ dpr: item.id });
+  }
+};
